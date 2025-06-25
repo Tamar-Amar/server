437e794e55bcb6ec97a4c618a662ddaf3b00cf30
Tamar-Amar <amtamar747@gmail.com>
2025-06-25 18:54:27 +0300

    made import by string

--- src/server.ts
+++ src/server.ts
@@ -62,5 +62,5 @@ app.get('/', (req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT as string;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
